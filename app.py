260d23a19f46904cfb241502e227032118f0ff9a
Satyam Vishwakarma <satyam.k.vishwakarma@gmail.com>
2025-06-27 12:22:14 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from beam import endpoint, Volume, Image
 import joblib
-import sklearn
+import numpy as np
 
 
 def load_model():
@@ -11,6 +11,15 @@ def load_model():
         return model
     except Exception as e:
         print(f"Error loading model: {e}")
+        return None
+
+def load_simple_model():
+    try:
+        model = joblib.load("./simplemodel.pkl")
+        print("Simple model loaded successfully.")
+        return model
+    except Exception as e:
+        print(f"Error loading simple model: {e}")
         return None
 
 @endpoint(
@@ -24,7 +33,7 @@ def load_model():
 def mlstockprice(context, **inputs):
     model = context.on_start_value
 
-    date = datetime.strptime(inputs["date"], "%d-%m-%Y")
+    date = datetime.strptime(inputs["date"], "%d/%m/%Y")
     year = date.year
     month = date.month
     day = date.day
@@ -35,5 +44,28 @@ def mlstockprice(context, **inputs):
     print(f"Prediction result: {result}")
     return {"result": result.tolist()}
 
+
+@endpoint(
+    cpu=0.8,
+    memory=1,
+    keep_warm_seconds=0,
+    name="mlstockpricesimple",
+    image=Image(python_packages=["joblib","numpy"]),
+    on_start=load_simple_model,
+)
+def mlstockprice_simple(context, **inputs):
+    model = context.on_start_value
+
+    date = datetime.strptime(inputs["date"], "%d/%m/%Y")
+    year = date.year
+    month = date.month
+    day = date.day
+
+    X= [[inputs["open"], inputs["high"], inputs["low"], year, month, day]]
+
+    result = np.dot(X, model['coef']) + model['intercept']
+    print(f"Prediction result: {result}")
+    return {"result": result.tolist()}
+
 if __name__ == "__main__":
-    mlstockprice.local
+    mlstockprice_simple.local
